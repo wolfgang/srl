a1419bb0df77fdcbd85c3f2ddf0b97ea6f2683d2
Wolfgang Deutsch <wolfgang.deutsch@gmail.com>
2021-05-21 10:45:52 +0200

    Add input "simulation" methods

--- src/_tests/helpers/input_simulator.rs
+++ src/_tests/helpers/input_simulator.rs
@@ -1,27 +1,50 @@
 use crate::input::Input;
 
-pub struct InputSimulator {}
+#[derive(Default)]
+pub struct InputSimulator {
+    simulating_move_left: bool,
+    simulating_move_right: bool,
+    simulating_move_up: bool,
+    simulating_move_down: bool,
+}
+
 
 impl InputSimulator {
     pub fn new() -> Self {
-        Self {}
+        Self::default()
+    }
+
+    pub fn simulate_move_left(&mut self) {
+        self.simulating_move_left = true
+    }
+
+    pub fn simulate_move_right(&mut self) {
+        self.simulating_move_right = true
+    }
+
+    pub fn simulate_move_up(&mut self) {
+        self.simulating_move_up = true
+    }
+
+    pub fn simulate_move_down(&mut self) {
+        self.simulating_move_down = true
     }
 }
 
 impl Input for InputSimulator {
     fn move_left(&self) -> bool {
-        false
+        self.simulating_move_left
     }
 
     fn move_right(&self) -> bool {
-        false
+        self.simulating_move_right
     }
 
     fn move_up(&self) -> bool {
-        false
+        self.simulating_move_up
     }
 
     fn move_down(&self) -> bool {
-        false
+        self.simulating_move_down
     }
 }
--- src/_tests/unit/input_simulator_test.rs
+++ src/_tests/unit/input_simulator_test.rs
@@ -10,3 +10,19 @@ fn initially_it_says_no_movement() {
     assert!(!input.move_up());
     assert!(!input.move_down());
 }
+
+#[test]
+fn simulate_move_x_causes_move_x_to_return_true() {
+    let mut input = InputSimulator::new();
+    input.simulate_move_left();
+    assert!(input.move_left());
+
+    input.simulate_move_right();
+    assert!(input.move_right());
+
+    input.simulate_move_up();
+    assert!(input.move_up());
+
+    input.simulate_move_down();
+    assert!(input.move_down());
+}
