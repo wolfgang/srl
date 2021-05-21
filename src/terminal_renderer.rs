06dcd45c4cfd8ebc39b640ae5474b9584a5f6df0
Wolfgang Deutsch <wolfgang.deutsch@gmail.com>
2021-05-21 12:02:00 +0200

    Sketch out TerminalRenderer

--- src/terminal_renderer.rs
+++ src/terminal_renderer.rs
@@ -1,20 +1,52 @@
-use std::io::Cursor;
+use std::io::Write;
 
+use crate::object_type::ObjectType::*;
 use crate::object_type::ObjectType;
 use crate::renderer::Renderer;
 
 pub struct TerminalRenderer {
-    render_buffer: Cursor<Vec<u8>>,
+    frame: Vec<Vec<char>>,
+    width: usize,
+    height: usize,
 }
 
 impl TerminalRenderer {
     pub fn new(width: usize, height: usize) -> Self {
-        Self { render_buffer: Cursor::new(Vec::with_capacity(width * height)) }
+        let mut renderer = Self {
+            width,
+            height,
+            frame: Vec::new(),
+        };
+        renderer.clear();
+        renderer
+    }
+
+    pub fn flush<T: Write>(&mut self, write: &mut T) {
+        write.write(self.frame_as_string().as_bytes()).unwrap();
+    }
+
+    fn frame_as_string(&self) -> String {
+        let strings: Vec<String> = self.frame
+            .iter()
+            .map(|row| { row.iter().collect() })
+            .collect();
+
+        return strings.join("\n");
     }
 }
 
 impl Renderer for TerminalRenderer {
-    fn clear(&mut self) {}
+    fn clear(&mut self) {
+        self.frame = vec![vec!['.'; self.width]; self.height]
+    }
 
-    fn render_at(&mut self, x: u32, y: u32, object_type: ObjectType) {}
+    fn render_at(&mut self, x: u32, y: u32, object_type: ObjectType) {
+        let ch = match object_type {
+            Wall => { '#' }
+            Player => { '@' }
+            Enemy => { 'E' }
+        };
+
+        self.frame[y as usize][x as usize] = ch;
+    }
 }
