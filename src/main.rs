2558b1185b173372248ba50f83fb564d84d4180d
Wolfgang Deutsch <wolfgang.deutsch@gmail.com>
2021-05-21 14:57:51 +0200

    Show cursor and clear screen before quitting

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,19 @@ use srl::game::GameConfig;
 use srl::gfx::terminal_renderer::TerminalRenderer;
 use srl::input::{Input, TerminalInput};
 
-const GAME_WIDTH: usize = 5;
-const GAME_HEIGHT: usize = 4;
+const GAME_WIDTH: usize = 40;
+const GAME_HEIGHT: usize = 20;
 
 fn main() {
-
     let config = GameConfig {
         dungeon_size: (GAME_WIDTH, GAME_HEIGHT)
     };
 
     let mut game = Game::new(config);
     game.add_enemies(&vec![(1, 1), (4, 2), (2, 3)]);
-    game.add_walls(&vec![(1, 0), (2, 0), (3, 1), (1, 2)]);
+    game.add_walls(&vec![
+        (1, 0), (2, 0), (3, 1), (1, 2),
+        (0, GAME_HEIGHT as u32 - 1), (GAME_WIDTH as u32 - 1, GAME_HEIGHT as u32 - 1)]);
     game.set_player_position(3, 2);
     let mut renderer = TerminalRenderer::new(GAME_WIDTH, GAME_HEIGHT);
     let mut input = TerminalInput::new();
@@ -28,7 +29,8 @@ fn main() {
         renderer.flush(&mut term);
         input.on_key(term.read_key().unwrap());
         game.tick(&input);
-        term.clear_last_lines(GAME_HEIGHT-1).unwrap();
+        term.clear_last_lines(GAME_HEIGHT - 1).unwrap();
     }
-
+    term.clear_to_end_of_screen().unwrap();
+    term.show_cursor().unwrap();
 }
