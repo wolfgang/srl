f2488c923f9d8e083a74efa4949491f92181818f
Wolfgang Deutsch <wolfgang.deutsch@gmail.com>
2021-06-15 09:11:55 +0200

    Remove some code duplication

--- src/_tests/unit/gfx/terminal_renderer_test.rs
+++ src/_tests/unit/gfx/terminal_renderer_test.rs
@@ -7,41 +7,38 @@ use crate::gfx::{Renderer, TerminalRenderer};
 #[test]
 fn flush_writes_empty_tiles() {
     let mut renderer = TerminalRenderer::new(3, 2);
-    let mut buffer: Cursor<Vec<u8>> = Cursor::new(Vec::new());
-    renderer.flush(&mut buffer);
-    let written_str = str::from_utf8(buffer.get_ref()).unwrap();
-    assert_eq!(written_str, "...\n...");
+    verify_flush_writes("...\n...", &mut renderer);
 }
 
 #[test]
 fn flush_writes_previously_rendered_tiles() {
     let mut renderer = TerminalRenderer::new(2, 2);
-    let mut buffer: Cursor<Vec<u8>> = Cursor::new(Vec::new());
 
     renderer.render_at(0, 0, Player);
     renderer.render_at(1, 0, Wall);
     renderer.render_at(0, 1, Enemy);
     renderer.render_at(1, 1, Floor);
 
-
-    renderer.flush(&mut buffer);
-    let written_str = str::from_utf8(buffer.get_ref()).unwrap();
-    assert_eq!(written_str, "@#\nE.");
+    verify_flush_writes("@#\nE.", &mut renderer);
 }
 
 #[test]
 fn clear_resets_tiles_to_empty() {
     let mut renderer = TerminalRenderer::new(2, 2);
-    let mut buffer: Cursor<Vec<u8>> = Cursor::new(Vec::new());
 
     renderer.render_at(0, 0, Player);
     renderer.render_at(1, 0, Wall);
     renderer.render_at(0, 1, Enemy);
     renderer.render_at(1, 1, Floor);
     renderer.clear();
-    renderer.flush(&mut buffer);
-    let written_str = str::from_utf8(buffer.get_ref()).unwrap();
-    assert_eq!(written_str, "..\n..");
 
-
+    verify_flush_writes("..\n..", &mut renderer);
 }
+
+fn verify_flush_writes(expected: &str, renderer: &mut TerminalRenderer) {
+    let mut buffer = Cursor::new(Vec::new());
+    renderer.flush(&mut buffer);
+    let actual = str::from_utf8(&buffer.get_ref()).unwrap();
+    assert_eq!(actual, expected);
+}
+
