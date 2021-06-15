7e53a0a70c06d2d333c4012fb5a1ea1a79c24d05
Wolfgang Deutsch <wolfgang.deutsch@gmail.com>
2021-06-15 11:10:24 +0200

    Implement ControlledCombatEngine::say_is_hit and is_hit

--- src/_tests/_helpers/controlled_combat_engine.rs
+++ src/_tests/_helpers/controlled_combat_engine.rs
@@ -1,21 +1,30 @@
+use std::collections::HashMap;
+
 use crate::combat::combat_engine::CombatEngine;
 use crate::game::dungeon::DungeonCoords;
 
-pub struct ControlledCombatEngine {}
+pub struct ControlledCombatEngine {
+    hits: HashMap<DungeonCoords, DungeonCoords>,
+}
 
 impl ControlledCombatEngine {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            hits: HashMap::new()
+        }
     }
 
-    pub fn say_is_hit(&mut self, _attacker: DungeonCoords, _attackee: DungeonCoords) {}
+    pub fn say_is_hit(&mut self, attacker: DungeonCoords, victim: DungeonCoords) {
+        self.hits.insert(attacker, victim);
+    }
 
     pub fn say_damage(&mut self, _attacker: DungeonCoords, _amount: u32) {}
 }
 
 impl CombatEngine for ControlledCombatEngine {
-    fn is_hit(&self, _attacker: (u32, u32), _victim: (u32, u32)) -> bool {
-        false
+    fn is_hit(&self, attacker: (u32, u32), victim: (u32, u32)) -> bool {
+        if !self.hits.contains_key(&attacker) { return false; }
+        self.hits.get(&attacker).unwrap() == &victim
     }
 
     fn roll_damage(&self, _attacker: (u32, u32)) -> u32 {
--- src/_tests/unit/_test_helpers/controlled_combat_engine_test.rs
+++ src/_tests/unit/_test_helpers/controlled_combat_engine_test.rs
@@ -2,12 +2,31 @@ use crate::_tests::_helpers::controlled_combat_engine::ControlledCombatEngine;
 use crate::combat::combat_engine::CombatEngine;
 use crate::game::dungeon::DungeonCoords;
 
-const ATTACKER: DungeonCoords = (1, 2);
-const VICTIM: DungeonCoords = (3, 4);
+const ATTACKER_1: DungeonCoords = (1, 2);
+const VICTIM_1: DungeonCoords = (3, 4);
+const ATTACKER_2: DungeonCoords = (5, 6);
+const VICTIM_2: DungeonCoords = (7, 8);
 
 #[test]
 fn initially_does_not_hit_and_does_not_roll_damage() {
     let engine = ControlledCombatEngine::new();
-    assert!(!engine.is_hit(ATTACKER, VICTIM));
-    assert_eq!(engine.roll_damage(ATTACKER), 0);
+    assert!(!engine.is_hit(ATTACKER_1, VICTIM_1));
+    assert!(!engine.is_hit(ATTACKER_2, VICTIM_2));
+    assert_eq!(engine.roll_damage(ATTACKER_1), 0);
+    assert_eq!(engine.roll_damage(ATTACKER_2), 0);
+}
+
+#[test]
+fn say_is_hit_causes_hit_for_given_attacker() {
+    let mut engine = ControlledCombatEngine::new();
+
+    engine.say_is_hit(ATTACKER_1, VICTIM_1);
+    assert!(engine.is_hit(ATTACKER_1, VICTIM_1));
+    assert!(!engine.is_hit(ATTACKER_2, VICTIM_2));
+
+    engine.say_is_hit(ATTACKER_2, VICTIM_2);
+    assert!(engine.is_hit(ATTACKER_1, VICTIM_1));
+    assert!(engine.is_hit(ATTACKER_2, VICTIM_2));
+
+
 }
