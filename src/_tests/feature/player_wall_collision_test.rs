c9cc21e5c2844dbcfca270aeb7a5e618313e0864
Wolfgang Deutsch <wolfgang.deutsch@gmail.com>
2021-06-17 09:34:42 +0200

    Add spaces for readability

--- src/_tests/feature/player_wall_collision_test.rs
+++ src/_tests/feature/player_wall_collision_test.rs
@@ -4,47 +4,45 @@ use crate::input::move_direction::MoveDirection::*;
 #[test]
 fn player_collides_with_north_wall() {
     let mut game = TestableGame::from_strings(vec![
-        "###",
-        ".@.",
+        "# # #",
+        ". @ .",
     ]);
 
     game.input.simulate_move(Up);
     game.verify_next_tiles(vec![
-        "###",
-        ".@.",
+        "# # #",
+        ". @ .",
     ]);
 }
-
-
 
 #[test]
 fn player_collides_with_west_wall() {
     let mut game = TestableGame::from_strings(vec![
-        "#.",
-        "#@",
-        "#.",
+        "# .",
+        "# @",
+        "# .",
     ]);
 
     game.input.simulate_move(Left);
     game.verify_next_tiles(vec![
-        "#.",
-        "#@",
-        "#.",
+        "# .",
+        "# @",
+        "# .",
     ]);
 }
 
 #[test]
 fn player_collides_with_east_wall() {
     let mut game = TestableGame::from_strings(vec![
-        "..#",
-        ".@#",
-        "..#",
+        ". . #",
+        ". @ #",
+        ". . #",
     ]);
 
     game.input.simulate_move(Right);
     game.verify_next_tiles(vec![
-        "..#",
-        ".@#",
-        "..#",
+        ". . #",
+        ". @ #",
+        ". . #",
     ]);
 }
