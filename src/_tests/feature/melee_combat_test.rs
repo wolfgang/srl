87d679ed048e30496b032c190aa3849082da9274
Wolfgang Deutsch <wolfgang.deutsch@gmail.com>
2021-07-11 09:54:27 +0200

    Add failing test for multiple hits

--- src/_tests/feature/melee_combat_test.rs
+++ src/_tests/feature/melee_combat_test.rs
@@ -3,7 +3,7 @@ use crate::_tests::_helpers::testable_game::TestableGame;
 use crate::input::move_direction::MoveDirection::Right;
 
 #[test]
-fn enemy_is_removed_when_dead() {
+fn enemy_is_removed_afer_one_hit() {
     let mut game = TestableGame::from_strings(vec![". @ E ."]);
     let mut combat_engine = ControlledCombatEngine::new();
     combat_engine.say_is_hit((1, 0), (2, 0));
@@ -13,3 +13,18 @@ fn enemy_is_removed_when_dead() {
     game.input.simulate_move(Right);
     game.verify_next_tiles(vec![". . @ ."]);
 }
+
+#[ignore]
+#[test]
+fn enemy_is_removed_after_two_hits() {
+    let mut game = TestableGame::from_strings(vec![". @ E ."]);
+    let mut combat_engine = ControlledCombatEngine::new();
+    combat_engine.say_is_hit((1, 0), (2, 0));
+    combat_engine.say_damage((1, 0), 60);
+
+    game.game.override_combat_engine(combat_engine);
+    game.input.simulate_move(Right);
+    game.verify_next_tiles(vec![". @ E ."]);
+    game.verify_next_tiles(vec![". . @ ."]);
+}
+
