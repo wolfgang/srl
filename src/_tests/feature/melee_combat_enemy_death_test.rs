b773a62438a7c4a9216abf1492b38cce5ec60a60
Wolfgang Deutsch <wolfgang.deutsch@gmail.com>
2021-07-11 18:33:04 +0200

    Extract constants for player and enemy coordinates

--- src/_tests/feature/melee_combat_enemy_death_test.rs
+++ src/_tests/feature/melee_combat_enemy_death_test.rs
@@ -1,12 +1,16 @@
 use crate::_tests::_helpers::testable_game::TestableGame;
+use crate::game::dungeon::DungeonCoords;
 use crate::input::move_direction::MoveDirection::Right;
+
+const PLAYER: DungeonCoords = (1, 0);
+const ENEMY: DungeonCoords = (2, 0);
 
 #[test]
 fn enemy_is_removed_after_one_hit() {
     let mut game = TestableGame::from_strings(vec![". @ E ."]);
     game.configure_combat(|combat_engine| {
-        combat_engine.say_is_hit((1, 0), (2, 0));
-        combat_engine.say_damage((1, 0), 1000);
+        combat_engine.say_is_hit(PLAYER, ENEMY);
+        combat_engine.say_damage(PLAYER, 1000);
     });
 
     game.input.simulate_move(Right);
@@ -17,9 +21,10 @@ fn enemy_is_removed_after_one_hit() {
 fn enemy_is_removed_after_two_hits() {
     let mut game = TestableGame::from_strings(vec![". @ E ."]);
     game.configure_combat(|combat_engine| {
-        combat_engine.say_is_hit((1, 0), (2, 0));
-        combat_engine.say_damage((1, 0), 60);
+        combat_engine.say_is_hit(PLAYER, ENEMY);
+        combat_engine.say_damage(PLAYER, 60);
     });
+
     game.input.simulate_move(Right);
     game.verify_next_tiles(vec![". @ E ."]);
     game.verify_next_tiles(vec![". . @ ."]);
@@ -29,8 +34,8 @@ fn enemy_is_removed_after_two_hits() {
 fn combat_log_reflects_enemy_death() {
     let mut game = TestableGame::from_strings(vec![". @ E ."]);
     game.configure_combat(|combat_engine| {
-        combat_engine.say_is_hit((1, 0), (2, 0));
-        combat_engine.say_damage((1, 0), 1000);
+        combat_engine.say_is_hit(PLAYER, ENEMY);
+        combat_engine.say_damage(PLAYER, 1000);
     });
 
     game.input.simulate_move(Right);
