cfae7ed930d0e44b31afdaeeec4eb99fdaeb495b
Wolfgang Deutsch <wolfgang.deutsch@gmail.com>
2021-07-30 09:10:49 +0200

    Add IDE file that appeared after the latest update

--- src/gfx/renderer.rs
+++ src/gfx/renderer.rs
@@ -4,5 +4,5 @@ pub trait Renderer {
     fn clear(&mut self);
     fn render_tile(&mut self, x: u32, y: u32, object_type: ObjectType);
     fn append_combat_log(&mut self, text: &str);
-    fn render_player_hp(&mut self, _value: i32);
+    fn render_player_hp(&mut self, value: i32);
 }
